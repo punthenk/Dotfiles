ca1292eaabfe6542358b86a0a2181f31ca7847d9
MichielN19 <Michielmitsjol@gmail.com>
2026-02-08 11:43:32 +0100

    add: config and setup files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask ghostty firefox karabiner-elements zed raycast
+
+brew install git neovim php tmux
